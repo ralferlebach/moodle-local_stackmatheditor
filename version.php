813aafa44848410783f1e8338549cc257fbd65a1
ralferlebach <ralf.erlebach@gmx.de>
2026-03-23 07:50:38 +0100

    Update version.php

--- version.php
+++ version.php
@@ -1,11 +1,11 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version      = 2026032221;
+$plugin->version      = 2026032300;
 $plugin->requires     = 2022112800;
 $plugin->component    = 'local_stackmatheditor';
 $plugin->maturity     = MATURITY_ALPHA;
-$plugin->release      = '0.8';
+$plugin->release      = '0.81';
 $plugin->dependencies = [
     'qtype_stack' => ANY_VERSION,
 ];
