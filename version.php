24864ce76e2f13087bc8b34aa84a7139c6865fde
ralferlebach <ralf.erlebach@gmx.de>
2026-03-26 11:36:48 +0100

    Fix Coding styles, syntax error

--- version.php
+++ version.php
@@ -24,12 +24,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version      = 2026032605;
-$plugin->requires     = 2024042200;  // Moodle 4.4 minimum (tested up to 4.5+).
+$plugin->version      = 2026032400;
+$plugin->requires     = 2024042200;
 $plugin->component    = 'local_stackmatheditor';
-$plugin->maturity     = MATURITY_BETA
-$plugin->release      = '0.98;
+$plugin->maturity     = MATURITY_ALPHA;
+$plugin->release      = '0.82';
 $plugin->dependencies = [
-    // Requires STACK 4.6 or later (supports Maxima 5.46+ and PHP 8.x).
     'qtype_stack' => 2024010400,
 ];
