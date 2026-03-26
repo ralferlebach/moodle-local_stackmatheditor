baf68a99dffd6ba6bfc95d3fbd35259906e54504
ralferlebach <ralf.erlebach@gmx.de>
2026-03-26 19:13:20 +0100

    re-run CI tests

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version      = 2026032609;
+$plugin->version      = 2026032610;
 $plugin->requires     = 2024042200;
 $plugin->component    = 'local_stackmatheditor';
 $plugin->maturity     = MATURITY_BETA;
